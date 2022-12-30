c58d523c88fb3221e7a6f6805779b05b37423278
Aigbodion Dominion <116392457+alexgrate@users.noreply.github.com>
2022-12-30 18:09:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,19 +5,16 @@ let homeSCORE = document.getElementById("home-score")
 let count = 0
 
 function plusone() {
-    console.log("clicked")
     count += 1
     homeSCORE.textContent = count
 }
 
 function plustwo() {
-    console.log("clicked")
     count += 2
     homeSCORE.textContent = count
 }
 
 function plusthree() {
-    console.log("clicked")
     count += 3
     homeSCORE.textContent = count
 }
@@ -28,19 +25,16 @@ let homesCORE = document.getElementById("home_score")
 let Count = 0 
 
 function plus_one() {
-    console.log("clicked")
     Count += 1
     homesCORE.textContent = Count
 }
 
 function plus_two() {
-    console.log("clicked")
     Count += 2
     homesCORE.textContent = Count
 }
 
 function plus_three() {
-    console.log("clicked")
     Count += 3
     homesCORE.textContent = Count
 }
