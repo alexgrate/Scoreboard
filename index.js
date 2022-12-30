a01186ff0591c002dcbe4eb6a9b9df1fcd95c4bf
Aigbodion Dominion <116392457+alexgrate@users.noreply.github.com>
2022-12-30 17:47:59 +0100

    Commit initial files

---
+++ index.js
@@ -0,0 +1,47 @@
+
+
+
+let homeSCORE = document.getElementById("home-score")
+let count = 0
+
+function plusone() {
+    console.log("clicked")
+    count += 1
+    homeSCORE.textContent = count
+}
+
+function plustwo() {
+    console.log("clicked")
+    count += 2
+    homeSCORE.textContent = count
+}
+
+function plusthree() {
+    console.log("clicked")
+    count += 3
+    homeSCORE.textContent = count
+}
+
+
+
+let homesCORE = document.getElementById("home_score")
+let Count = 0 
+
+function plus_one() {
+    console.log("clicked")
+    Count += 1
+    homesCORE.textContent = Count
+}
+
+function plus_two() {
+    console.log("clicked")
+    Count += 2
+    homesCORE.textContent = Count
+}
+
+function plus_three() {
+    console.log("clicked")
+    Count += 3
+    homesCORE.textContent = Count
+}
+
